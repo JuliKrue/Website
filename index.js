eaf59b916f47aee466547f7bf143338d27383d9d
kruejuli <kruejuli@students.uni-mainz.de>
2022-02-03 14:38:32 +0100

    'deleted bootstrap, added new colors,mobile first'

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
-    strings: ['Digital Humanist.', 'Aspiring Webdeveloper.', 'Sounddesigner.', 'Bookworm.', 'Humble Cat & Corgi servant.', 'Runner & Crossfitter.'],
+    strings: ['Digital Humanist.', 'Aspiring Webdeveloper.', 'Sounddesigner.', 'Bookworm.', 'Humble Cat & Corgi servant.'],
     autoStart: true,
     loop: true,
 });
