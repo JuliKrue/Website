f8d7434b0eedea23705e7285dcab359fd33da559
kruejuli <kruejuli@students.uni-mainz.de>
2022-02-03 10:39:54 +0100

    'html structure, css style and typewriter.js added'

---
+++ index.js
@@ -0,0 +1,7 @@
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    strings: ['Digital Humanist.', 'Aspiring Webdeveloper.', 'Sounddesigner.', 'Bookworm.', 'Humble Cat & Corgi servant.', 'Runner & Crossfitter.'],
+    autoStart: true,
+    loop: true,
+});
