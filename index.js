d2ff6aeff57b958474930c2658e8c5bb0fcbab7b
julikrue <juli.krueger@outlook.de>
2022-02-04 11:06:39 +0100

    Added About section, deleted typewriter.js

--- index.js
+++
@@ -1,7 +0,0 @@
-var app = document.getElementById('app');
-
-var typewriter = new Typewriter(app, {
-    strings: ['Digital Humanist.', 'Aspiring Webdeveloper.', 'Sounddesigner.', 'Bookworm.', 'Humble Cat & Corgi servant.'],
-    autoStart: true,
-    loop: true,
-});
